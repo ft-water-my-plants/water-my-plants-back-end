fe179f73d1697366b8cc3656397dd4c93c469ceb
Bradly Lewis <bradlylewisdev@gmail.com>
2021-09-21 19:40:29 -0400

    wip

--- api/plants/plants-router.js
+++ api/plants/plants-router.js
@@ -7,25 +7,25 @@ const { restricted } = require("../auth/auth-middleware");
 
 router.get('/', (req, res, next) => {
     Plant.find()
-    .then(plants=>{
+    .then(plants => {
         res.json(plants)
     })
     .catch(next)
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', restricted, (req, res, next) => {
     Plant.findById(req.params.id)
-    .then(plants=>{
+    .then(plants => {
         res.json(plants)
     })
     .catch(next)
 })
 
 
-router.delete('/:plant_id', (req,res,next) =>{
+router.delete('/:plant_id', (req, res, next) =>{
     Plant.del(req.params.plant_id)
-    .then(obj=>{
-        res.json(obj)
+    .then(plant => {
+        res.json(plant)
     })
     .catch(next)
 })
